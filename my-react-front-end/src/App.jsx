7d3f9bdb75ce41c8024a9f10dbdddaef3f713c82
viktorriznik73-blip <viktorriznik73@gmail.com>
2026-06-14 18:50:25 +0200

    First commit: starting React

---
+++ my-react-front-end/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ const myvariable = "Hello"
+
+  return (
+   <div>
+  <h1 className='hi'>{myvariable}</h1>
+   </div>
+  )
+}
+
+export default App
